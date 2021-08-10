6763a510d5e15b438d8690c5874726294b9d445a
Tamir Cohen <tamircohen2468@gmail.com>
2021-08-10 18:12:47 +0300

    finshed client and server tls handshake

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Rabbit TLS
-using tls communication for rabbit service
+using tls communication for rabbit service.  
+Enables Encrypted communication with peer verification, reed more at [Rabbit TLS](https://www.rabbitmq.com/ssl.html).
 
 ## Requirements
 ### 1. Rust develop env
@@ -15,13 +16,40 @@ Win 10 docker installation [guide](https://www.ntweekly.com/2018/04/28/install-d
 sudo apt install docker.io
 ```
 
+### 3. Add CA
+you need to add the generated CA certificate to the considered trusted certificates.
+#### **windows**
+On Windows trusted certificates are managed using tools such as [certmgr](https://docs.microsoft.com/en-us/dotnet/framework/tools/certmgr-exe-certificate-manager-tool).  
+The Certificate Manager is automatically installed with Visual Studio.
+```shell
+cd "C:\Program Files (x86)\Windows Kits\10\bin\10.0. 16299.0\x64"
+certmgr -add -all "<repo path>\certificate\ca_certificate.pem" -s Root
+```
+#### **Linux**
+```shell
+# Linux with Mono
+certmgr -add -c Trust <repo path>/certificate/ca_certificate.pem
+```
+
 ## Rabbit server setup
 ```shell
 make run-docker
 ```
-This command runs the Rabbit server management plugin, and enables managment interface on port 8080. You can then go to http://localhost:8080 with the default username and password of  ```guest/guest```.
+This command runs the Rabbit server with TLS support, also enables managment interface on port 8080.  
+You can then go to http://localhost:8080 with the default username and password of  ```guest/guest```.
 
 For more capabilities see [rabbitmq on dockerhub](https://hub.docker.com/_/rabbitmq)
 
+## Using tls-gen's Basic Profile
+Below is an example that generates a CA and uses it to produce two certificate/key pairs, one for the server and another for clients. This is the setup for the certificate directory.  
+**Do not** genarate certificate with password, the client can't read them.
+```shell
+git clone https://github.com/michaelklishin/tls-gen tls-gen
+cd tls-gen/basic
+make PYTHON=python
+```
 
-
+## Use OpenSSL Tools to Test TLS Connection
+```shell
+openssl s_client -connect localhost:5671 -cert client_certificate.pem -key client_key.pem -CAfile ca_certificate.pem
+```
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,121 @@
-use amiquip::{Auth, Connection, ConnectionOptions, ConnectionTuning, Exchange, Publish, Result};
-use log::LevelFilter;
+use amiquip::{Auth, Connection, ConnectionOptions, ConnectionTuning, Exchange, Publish};
+use log::{info, LevelFilter};
 use mio::net::TcpStream;
-use native_tls::{Certificate, TlsConnector};
+use native_tls::{Certificate, Identity, TlsConnector};
 use simple_logger::SimpleLogger;
-use std::{
-    fs,
-    net::{SocketAddr, SocketAddrV4},
-    path::Path,
-};
+use std::{fs, path::Path};
 
-struct RabbitTLS<P, U, S> {
-    certificate: P,
-    url: U,
-    addr: S,
+// certificate 'Common Name' was set on cration, do not touch this!
+const C_CA_CN: &str = "DESKTOP-G7H3D49";
+const C_RABBIT_IP: &str = "127.0.0.1";
+const C_RABBIT_PORT: u16 = 5671;
+const C_CLIENT_CERT: &str = r"certificate\client_certificate.pem";
+const C_CLIENT_P12: &str = r"certificate\client_key.p12";
+
+// Rabbit configuration struct
+#[derive(Debug)]
+struct Rabbit<P: AsRef<Path>> {
+    username: String,
+    password: String,
+    host: String,
+    port: u16,
+    client_cert: P,
+    client_p12: P,
 }
 
-impl<P, U, S> RabbitTLS<P, U, S>
-where
-    P: AsRef<Path>,
-    U: AsRef<str>,
-    S: Into<SocketAddr>,
-{
-    fn new(secure: bool, auth: Auth, host: &str, port: usize, ca_file: P) -> Self {}
+impl<P: AsRef<Path>> Rabbit<P> {
+    fn new(
+        username: &str,
+        password: &str,
+        host: &str,
+        port: u16,
+        client_cert: P,
+        client_p12: P,
+    ) -> Self {
+        Self {
+            username: username.to_owned(),
+            password: password.to_owned(),
+            host: host.to_owned(),
+            port,
+            client_cert,
+            client_p12,
+        }
+    }
+
+    // get connection options
+    fn get_conn_options(&self) -> ConnectionOptions<Auth> {
+        ConnectionOptions::default().auth(Auth::Plain {
+            username: self.username.clone(),
+            password: self.password.clone(),
+        })
+    }
+
+    // get TLS connector configurad with needed certifications
+    fn get_connector(&self) -> TlsConnector {
+        let cert_byte_vec = fs::read(&self.client_cert).expect("failed to load cert");
+        let cert = Certificate::from_pem(&cert_byte_vec).expect("failed to parse cert");
+
+        let identity_byte_vec = fs::read(&self.client_p12).expect("failed to load client_p12");
+        let identity =
+            Identity::from_pkcs12(&identity_byte_vec, "").expect("failed to parse client_p12");
+
+        TlsConnector::builder()
+            .identity(identity)
+            .add_root_certificate(cert)
+            .build()
+            .unwrap()
+    }
+
+    // open tcp stream on the rabbit port
+    fn get_stream(&self) -> TcpStream {
+        TcpStream::connect(&format!("{}:{}", self.host, self.port).parse().unwrap()).unwrap()
+    }
 }
-//     fn get_url(&self) -> String {
-//         format!(
-//             "{protocol}://{username}:{password}@{host}:{port}",
-//             protocol = if self.secure { "amqps" } else { "amqp" },
-//             username = self.username,
-//             password = self.password,
-//             host = self.host,
-//             port = self.port,
-//         )
-//     }
-
-//     fn get_stream(&self) -> TcpStream {
-//         let address = format!("{host}:{port}", host = self.host, port = self.port,);
-//         TcpStream::connect(&address.parse().unwrap()).unwrap()
-//     }
-
-//     fn get_cert(&self) -> Certificate {
-//         let cert_byte_vec = fs::read(self.cafile.clone()).expect("failed to load cert");
-//         Certificate::from_pem(&cert_byte_vec).expect("failed to parse cert")
-//     }
-// }
 
 fn main() {
-    // SimpleLogger::new()
-    //     .with_level(LevelFilter::Debug)
-    //     .init()
-    //     .unwrap();
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
 
-    // let rabbit = Rabbit {
-    //     host: "localhost".to_owned(),
-    //     port: 5671,
-    //     username: "guest".to_owned(),
-    //     password: "guest".to_owned(),
-    //     secure: true,
-    //     cafile: r"C:\Users\Tamir Cohen\Desktop\rabbit_tls\certificate\client_certificate.pem"
-    //         .to_owned(),
-    // };
-    // let connector = TlsConnector::builder()
-    //     .add_root_certificate(rabbit.get_cert())
-    //     .build()
-    //     .unwrap();
+    let rabbit = Rabbit::new(
+        "guest",
+        "guest",
+        C_RABBIT_IP,
+        C_RABBIT_PORT,
+        C_CLIENT_CERT,
+        C_CLIENT_P12,
+    );
 
-    // // Open connection.
-    // let mut connection = Connection::open_tls_stream::<Auth, TlsConnector, TcpStream>(
-    //     connector,
-    //     &rabbit.host,
-    //     rabbit.get_stream(),
-    //     ConnectionOptions::default(),
-    //     ConnectionTuning::default(),
-    // )
-    // .expect("Connection failed");
+    // client-side TLS connection
+    let connector = rabbit.get_connector();
+    let stream = rabbit.get_stream();
+    info!("Connected to TCP stream");
 
-    // // Open a channel - None says let the library choose the channel ID.
-    // let channel = connection
-    //     .open_channel(None)
-    //     .expect("Opening channel failed");
+    // Open connection.
+    let mut connection = Connection::open_tls_stream::<Auth, TlsConnector, TcpStream>(
+        connector,
+        C_CA_CN,
+        stream,
+        rabbit.get_conn_options(),
+        ConnectionTuning::default(),
+    )
+    .expect("Rabbit Connection failed");
+    info!("TLS connection successful");
 
-    // // Get a handle to the direct exchange on our channel.
-    // let exchange = Exchange::direct(&channel);
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection
+        .open_channel(None)
+        .expect("Opening channel failed");
 
-    // // Publish a message to the "hello" queue.
-    // exchange
-    //     .publish(Publish::new(b"hello there", "hello"))
-    //     .expect("Publish had failed");
+    // Get a handle to the direct exchange on our channel.
+    let exchange = Exchange::direct(&channel);
 
-    // connection.close().expect("Failed closing connection");
+    // Publish a message to the "hello" queue.
+    exchange
+        .publish(Publish::new(b"hello there", "hello"))
+        .expect("Publish had failed");
+
+    info!("message publish successful");
+    connection.close().expect("Failed closing connection");
 }
